ca98aa02bb17c1e4d3dea686002e548e0bcfcee6
Nadav Avital <nadav.avital@icloud.com>
2025-12-25 13:12:32 -0800

    Initial Commit

---
+++ Plates/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Plates
+//
+//  Created by Nadav Avital on 12/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Plates/PlatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlatesApp.swift
+//  Plates
+//
+//  Created by Nadav Avital on 12/25/25.
+//
+
+import SwiftUI
+
+@main
+struct PlatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
